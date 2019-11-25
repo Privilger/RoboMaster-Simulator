302d794f086e8025fb8e3cec89d27e97d18458ac
yizheng <zhangyizheng1996@outlook.com>
2019-11-25 19:27:58 +0800

    add openai_ros pkg

---
+++ src/openai_ros/openai_ros/src/openai_ros/gazebo_connection.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+import rospy
+from std_srvs.srv import Empty
+from gazebo_msgs.msg import ODEPhysics
+from gazebo_msgs.srv import SetPhysicsProperties, SetPhysicsPropertiesRequest
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Vector3
+
+class GazeboConnection():
+    
+    def __init__(self, start_init_physics_parameters, reset_world_or_sim):
+        
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self.reset_simulation_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        self.reset_world_proxy = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+
+        # Setup the Gravity Controle system
+        service_name = '/gazebo/set_physics_properties'
+        rospy.logdebug("Waiting for service " + str(service_name))
+        rospy.wait_for_service(service_name)
+        rospy.logdebug("Service Found " + str(service_name))
+
+        self.set_physics = rospy.ServiceProxy(service_name, SetPhysicsProperties)
+        self.start_init_physics_parameters = start_init_physics_parameters
+        self.reset_world_or_sim = reset_world_or_sim
+        self.init_values()
+        # We always pause the simulation, important for legged robots learning
+        self.pauseSim()
+
+    def pauseSim(self):
+        rospy.logdebug("PAUSING START")
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            self.pause()
+        except rospy.ServiceException as e:
+            print ("/gazebo/pause_physics service call failed")
+            
+        rospy.logdebug("PAUSING FINISH")
+        
+    def unpauseSim(self):
+        rospy.logdebug("UNPAUSING START")
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            self.unpause()
+        except rospy.ServiceException as e:
+            print ("/gazebo/unpause_physics service call failed")
+        
+        rospy.logdebug("UNPAUSING FiNISH")
+        
+    
+    def resetSim(self):
+        """
+        This was implemented because some simulations, when reseted the simulation
+        the systems that work with TF break, and because sometime we wont be able to change them
+        we need to reset world that ONLY resets the object position, not the entire simulation
+        systems.
+        """
+        if self.reset_world_or_sim == "SIMULATION":
+            rospy.logerr("SIMULATION RESET")
+            self.resetSimulation()
+        elif self.reset_world_or_sim == "WORLD":
+            rospy.logerr("WORLD RESET")
+            self.resetWorld()
+        elif self.reset_world_or_sim == "NO_RESET_SIM":
+            rospy.logerr("NO RESET SIMULATION SELECTED")
+        else:
+            rospy.logerr("WRONG Reset Option:"+str(self.reset_world_or_sim))
+    
+    def resetSimulation(self):
+        rospy.wait_for_service('/gazebo/reset_simulation')
+        try:
+            self.reset_simulation_proxy()
+        except rospy.ServiceException as e:
+            print ("/gazebo/reset_simulation service call failed")
+
+    def resetWorld(self):
+        rospy.wait_for_service('/gazebo/reset_world')
+        try:
+            self.reset_world_proxy()
+        except rospy.ServiceException as e:
+            print ("/gazebo/reset_world service call failed")
+
+    def init_values(self):
+
+        self.resetSim()
+
+        if self.start_init_physics_parameters:
+            rospy.logdebug("Initialising Simulation Physics Parameters")
+            self.init_physics_parameters()
+        else:
+            rospy.logerr("NOT Initialising Simulation Physics Parameters")
+        
+    def init_physics_parameters(self):
+        """
+        We initialise the physics parameters of the simulation, like gravity,
+        friction coeficients and so on.
+        """
+        self._time_step = Float64(0.001)
+        self._max_update_rate = Float64(1000.0)
+
+        self._gravity = Vector3()
+        self._gravity.x = 0.0
+        self._gravity.y = 0.0
+        self._gravity.z = -9.81
+
+        self._ode_config = ODEPhysics()
+        self._ode_config.auto_disable_bodies = False
+        self._ode_config.sor_pgs_precon_iters = 0
+        self._ode_config.sor_pgs_iters = 50
+        self._ode_config.sor_pgs_w = 1.3
+        self._ode_config.sor_pgs_rms_error_tol = 0.0
+        self._ode_config.contact_surface_layer = 0.001
+        self._ode_config.contact_max_correcting_vel = 0.0
+        self._ode_config.cfm = 0.0
+        self._ode_config.erp = 0.2
+        self._ode_config.max_contacts = 20
+
+        self.update_gravity_call()
+        
+
+    def update_gravity_call(self):
+
+        self.pauseSim()
+
+        set_physics_request = SetPhysicsPropertiesRequest()
+        set_physics_request.time_step = self._time_step.data
+        set_physics_request.max_update_rate = self._max_update_rate.data
+        set_physics_request.gravity = self._gravity
+        set_physics_request.ode_config = self._ode_config
+
+        rospy.logdebug(str(set_physics_request.gravity))
+
+        result = self.set_physics(set_physics_request)
+        rospy.logdebug("Gravity Update Result==" + str(result.success) + ",message==" + str(result.status_message))
+
+        self.unpauseSim()
+
+    def change_gravity(self, x, y, z):
+        self._gravity.x = x
+        self._gravity.y = y
+        self._gravity.z = z
+
+        self.update_gravity_call()
---
+++ src/openai_ros/openai_ros/src/openai_ros/task_envs/task_envs_list.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+from gym.envs.registration import register
+from gym import envs
+
+
+def RegisterOpenAI_Ros_Env(task_env, timestep_limit_per_episode=10000):
+    """
+    Registers all the ENVS supported in OpenAI ROS. This way we can load them
+    with variable limits.
+    Here is where you have to PLACE YOUR NEW TASK ENV, to be registered and accesible.
+    return: False if the Task_Env wasnt registered, True if it was.
+    """
+
+    ###########################################################################
+    # MovingCube Task-Robot Envs
+
+    result = True
+
+    # Cubli Moving Cube
+    if task_env == 'MovingCubeOneDiskWalk-v0':
+        # We register the Class through the Gym system
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.moving_cube.one_disk_walk.MovingCubeOneDiskWalkEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # We have to import the Class that we registered so that it can be found afterwards in the Make
+        from openai_ros.task_envs.moving_cube import one_disk_walk
+
+    # Husarion Robot
+    elif task_env == 'HusarionGetToPosTurtleBotPlayGround-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.husarion.husarion_get_to_position_turtlebot_playground.HusarionGetToPosTurtleBotPlayGroundEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.husarion import husarion_get_to_position_turtlebot_playground
+
+    elif task_env == 'FetchTest-v0':
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.fetch.fetch_test_task.FetchTestEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # 50
+        # We have to import the Class that we registered so that it can be found afterwards in the Make
+        from openai_ros.task_envs.fetch import fetch_test_task
+
+    elif task_env == 'FetchSimpleTest-v0':
+        register(
+            id=task_env,
+            # entry_point='openai_ros:task_envs.fetch.fetch_simple_task.FetchSimpleTestEnv',
+            entry_point='openai_ros.task_envs.fetch.fetch_simple_task:FetchSimpleTestEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # We have to import the Class that we registered so that it can be found afterwards in the Make
+        from openai_ros.task_envs.fetch import fetch_simple_task
+
+    elif task_env == 'FetchPickAndPlace-v0':
+        register(
+            id=task_env,
+            # entry_point='openai_ros:task_envs.fetch.fetch_pick_and_place_task.FetchPickAndPlaceEnv',
+            entry_point='openai_ros:task_envs.fetch.fetch_pick_and_place_task.FetchPickAndPlaceEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # We have to import the Class that we registered so that it can be found afterwards in the Make
+        from openai_ros.task_envs.fetch import fetch_pick_and_place_task
+
+    elif task_env == 'FetchPush-v0':
+        register(
+            id=task_env,
+            # entry_point='openai_ros:task_envs.fetch.fetch_pick_and_place_task.FetchPushEnv',
+            # entry_point='openai_ros:task_envs.fetch.fetch_push.FetchPushEnv',
+            entry_point='openai_ros.task_envs.fetch.fetch_push:FetchPushEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # We have to import the Class that we registered so that it can be found afterwards in the Make
+        from openai_ros.task_envs.fetch import fetch_push
+
+    elif task_env == 'CartPoleStayUp-v0':
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.cartpole_stay_up.stay_up.CartPoleStayUpEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.cartpole_stay_up import stay_up
+
+    elif task_env == 'HopperStayUp-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.hopper.hopper_stay_up.HopperStayUpEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.hopper import hopper_stay_up
+
+    elif task_env == 'IriWamTcpToBowl-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.iriwam.tcp_to_bowl.IriWamTcpToBowlEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.iriwam import tcp_to_bowl
+
+    elif task_env == 'ParrotDroneGoto-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.parrotdrone.parrotdrone_goto.ParrotDroneGotoEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.parrotdrone import parrotdrone_goto
+
+    elif task_env == 'SawyerTouchCube-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.sawyer.learn_to_touch_cube.SawyerTouchCubeEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.sawyer import learn_to_touch_cube
+
+    elif task_env == 'ShadowTcGetBall-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.shadow_tc.learn_to_pick_ball.ShadowTcGetBallEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.shadow_tc import learn_to_pick_ball
+
+    elif task_env == 'SumitXlRoom-v0':
+
+        register(
+            id='SumitXlRoom-v0',
+            entry_point='openai_ros:task_envs.sumit_xl.sumit_xl_room.SumitXlRoom',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.sumit_xl import sumit_xl_room
+
+    elif task_env == 'MyTurtleBot2Maze-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.turtlebot2.turtlebot2_maze.TurtleBot2MazeEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # import our training environment
+        from openai_ros.task_envs.turtlebot2 import turtlebot2_maze
+
+    elif task_env == 'MyTurtleBot2Wall-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.turtlebot2.turtlebot2_wall.TurtleBot2WallEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+        # import our training environment
+        from openai_ros.task_envs.turtlebot2 import turtlebot2_wall
+
+    elif task_env == 'TurtleBot3World-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.turtlebot3.turtlebot3_world.TurtleBot3WorldEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.turtlebot3 import turtlebot3_world
+
+    elif task_env == 'WamvNavTwoSetsBuoys-v0':
+
+        register(
+            id=task_env,
+            entry_point='openai_ros:task_envs.wamv.wamv_nav_twosets_buoys.WamvNavTwoSetsBuoysEnv',
+            timestep_limit=timestep_limit_per_episode,
+        )
+
+        # import our training environment
+        from openai_ros.task_envs.wamv import wamv_nav_twosets_buoys
+
+    # Add here your Task Envs to be registered
+    else:
+        result = False
+
+    ###########################################################################
+
+    if result:
+        # We check that it was really registered
+        supported_gym_envs = GetAllRegisteredGymEnvs()
+        #print("REGISTERED GYM ENVS===>"+str(supported_gym_envs))
+        assert (task_env in supported_gym_envs), "The Task_Robot_ENV given is not Registered ==>" + \
+            str(task_env)
+
+    return result
+
+
+def GetAllRegisteredGymEnvs():
+    """
+    Returns a List of all the registered Envs in the system
+    return EX: ['Copy-v0', 'RepeatCopy-v0', 'ReversedAddition-v0', ... ]
+    """
+
+    all_envs = envs.registry.all()
+    env_ids = [env_spec.id for env_spec in all_envs]
+
+    return env_ids
